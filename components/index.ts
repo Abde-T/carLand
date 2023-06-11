d49fc2691376dfbca8d2132551afc7bfda6977b0
Abderrahmane <tiamani3939@gmail.com>
2023-06-11 11:18:48 +0100

    landing page

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from './CustomButton';
+import Nav from "./Nav";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar"; 
+import CustomFilter from "./CustomFilter";
+import SearchManuFacturer from "./SearchManuFacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+
+export {
+    Hero,
+    CustomButton,
+    Nav,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManuFacturer,
+    CarCard,
+    CarDetails
+}
