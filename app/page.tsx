09dfb8957354246efb9e887467f165e38eb28ec6
Abderrahmane <tiamani3939@gmail.com>
2023-06-15 11:18:11 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home({searchParams}: HomeProps) {
     <div className='mt-12 padding-x padding-y max-width' id='discover'>
       <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'> Car Catalogue</h1>
-          <p>Exlpore the cars you might like</p>
+          <p>Explore the cars you might like</p>
       </div>
       <div className='home__filters'>
           <SearchBar/>
